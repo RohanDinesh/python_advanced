2438c4f236801f2a8e941871ffc06b7a48232463
Rohan <66969681+RohanDinesh@users.noreply.github.com>
2021-04-11 21:23:09 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-a = input("enter value of a")
+var = input("enter value of var")
 
 
 def check(b):
     if int(b) in range(1, 10):
         print('a is between 1-9')
     else:
-        print('a is outside specified range')
+        print('a is outside specified range ie., outside 1-9')
 
 
+check(var)
 
-check(a)
